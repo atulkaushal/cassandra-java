1f3af388c8c0058a5655c5f36ecacdc39e05ea31
atul kaushal <atul.hkaushal@gmail.com>
2021-04-30 17:05:38 -0500

    added another way to create session.

--- src/main/java/com/github/atulkaushal/cassandra/Main.java
+++ src/main/java/com/github/atulkaushal/cassandra/Main.java
@@ -1,10 +1,14 @@
 package com.github.atulkaushal.cassandra;
 
+import java.net.InetSocketAddress;
+
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
 
-/** The Class Main. */
+/** The Class Main. 
+ * @author Atul
+ * */
 public class Main {
 
   /**
@@ -15,11 +19,20 @@ public class Main {
   public static void main(String[] args) {
 
     // If you don’t specify any contact point, the driver defaults to 127.0.0.1:9042
-    CqlSession session = CqlSession.builder().build();
+    // CqlSession session = CqlSession.builder().build();
+
+    // CQLsession with default Keyspace
+    CqlSession session =
+        CqlSession.builder()
+            .addContactPoint(new InetSocketAddress("localhost", 9042))
+            .withLocalDatacenter("datacenter1")
+            .withKeyspace("reservation")
+            .build();
 
     ResultSet rs = session.execute("select release_version from system.local");
-    Row row = rs.one();
-    System.out.println(row.getString("release_version"));
+    Row row0 = rs.one();
+    System.out.println(row0.getString("release_version"));
+
     session.close();
   }
 }
